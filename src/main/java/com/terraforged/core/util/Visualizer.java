82bcd434921697fb51fd272d65ed6f10ec79ab22
dags- <dags@dags.me>
2020-06-08 13:39:24 +0100

    - fix forest surface placing over water

--- src/main/java/com/terraforged/core/util/Visualizer.java
+++ src/main/java/com/terraforged/core/util/Visualizer.java
@@ -2,7 +2,6 @@ package com.terraforged.core.util;
 
 import me.dags.noise.Module;
 import me.dags.noise.Source;
-import me.dags.noise.util.NoiseUtil;
 
 import javax.swing.*;
 import java.awt.*;
@@ -12,15 +11,14 @@ public class Visualizer {
 
     public static void main(String[] args) {
         int size = 512;
-        Module noise = Source.perlin(123, 200, 1).steps(5, 0.45, 0.6F);
+        Module noise = Source.simplex(123, 40, 2).warp(Source.RAND, 124, 2, 1, 4);
+
         BufferedImage image = new BufferedImage(size, size, BufferedImage.TYPE_INT_RGB);
-        PosIterator iterator = PosIterator.area(0, 0, size, 1);
+
+        PosIterator iterator = PosIterator.area(0, 0, size, size);
         while (iterator.next()) {
             float value = noise.getValue(iterator.x(), iterator.z());
-            int height = NoiseUtil.round((image.getHeight() - 1) * value);
-            for (int y = 0; y < height; y++) {
-                image.setRGB(iterator.x(), image.getHeight() - 1 - y, 0xffffff);
-            }
+            image.setRGB(iterator.x(), iterator.z(), getMaterial(value));
         }
 
         JFrame frame = new JFrame();
