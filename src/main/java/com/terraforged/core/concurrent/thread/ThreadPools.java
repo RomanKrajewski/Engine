7a8cf4023c45668b7622aee8b372e119c998a6a7
dags- <dags@dags.me>
2020-06-14 12:25:09 +0100

    added biome weight & performance configs

--- src/main/java/com/terraforged/core/concurrent/thread/ThreadPools.java
+++ src/main/java/com/terraforged/core/concurrent/thread/ThreadPools.java
@@ -2,18 +2,31 @@ package com.terraforged.core.concurrent.thread;
 
 public class ThreadPools {
 
-    private static final ThreadPool instance = create(defaultPoolSize());
+    private static final ThreadPool instance = createDefaultPool(defaultPoolSize());
 
     public static ThreadPool getPool() {
         return instance;
     }
 
-    public static ThreadPool create(int poolSize) {
+    private static ThreadPool createDefaultPool(int poolSize) {
         if (poolSize == 1) {
             return new SingleThreadPool();
         }
         if (poolSize < 4) {
             return new ForkJoinThreadPool(poolSize);
+        }
+        return BatchingThreadPool.of(poolSize);
+    }
+
+    public static ThreadPool create(int poolSize, boolean batching) {
+        if (poolSize == 1) {
+            return new SingleThreadPool();
+        }
+        if (poolSize < 4 || !batching) {
+            return new ForkJoinThreadPool(poolSize);
+        }
+        if (poolSize == instance.size() && instance.supportsBatching()) {
+            return instance;
         }
         return BatchingThreadPool.of(poolSize);
     }
